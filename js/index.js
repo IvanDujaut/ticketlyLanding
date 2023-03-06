2a0fa003b25efbab5610ad463cf01920c5144bb1
Ivan Dujaut <ivan.dujaut@garajedeideas.com>
2023-03-06 20:08:07 -0300

    [FEAT] - exercises 1

---
+++ js/index.js
@@ -0,0 +1,33 @@
+let name = "Homero";
+let lastName = "Simpson";
+let age = 67;
+
+const city1 = "Posadas";
+const city2 = "Corrientes";
+const city3 = "CABA";
+const city4 = "Montevideo";
+const city5 = "Brasilia";
+
+let drivingLicenseName = "Bart";
+let drivingLicenseLastName = "Simpson";
+let drivingLicenseAddress = "742 Evergreen Terr.";
+let drivingLicenseCity = "Springfield";
+let drivingLicenseCountry = "USA";
+let drivingLicenseCode = "B47U89RE243";
+let gender = "M";
+
+const drivingLicense =
+  drivingLicenseCode +
+  "\n" +
+  drivingLicenseName +
+  " " +
+  drivingLicenseLastName +
+  "\n" +
+  drivingLicenseAddress +
+  "\n" +
+  drivingLicenseCity +
+  ", " +
+  drivingLicenseCountry;
+
+
+console.log(drivingLicense);
